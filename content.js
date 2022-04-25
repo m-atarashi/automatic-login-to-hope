a606352bcdb13add509ce5c451b1b90ce6d27f8a
Masamitsu Atarashi <b1018235@fun.ac.jp>
2022-04-25 18:45:25 +0900

    fix: spacing

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ void (async function main () {
     const loginPagePattern = /https:\/\/hope\.fun\.ac\.jp(\/local\/hope\/login\.php.*)?/
     if (location.href.match(loginPagePattern)) {
         location.href = 'https://hope.fun.ac.jp/auth/saml2/login.php?idp=1bec319bca7458548c77d545a2a1b3de'
-    }else if (location.href === 'https://sso.fun.ac.jp/my.policy') {
+    } else if (location.href === 'https://sso.fun.ac.jp/my.policy') {
         automaticLogin()
     }
 })()
